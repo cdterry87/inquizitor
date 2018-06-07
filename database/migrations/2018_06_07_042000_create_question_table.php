709d8b079526a8269ff1d417942c3a6e3ecd0392
= <chase.terry87@gmail.com>
2018-06-07 13:36:24 -0500

    Setup database migrations.

---
+++ database/migrations/2018_06_07_042000_create_question_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('question', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('quiz_id')->unsigned()->index();
+            $table->foreign('quiz_id')->references('id')->on('quiz');
+            $table->integer('answer_id')->unsigned()->index();
+            $table->foreign('answer_id')->references('id')->on('answer');
+            $table->string('image');
+            $table->text('video');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('question');
+    }
+}
